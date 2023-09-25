7a978f1669656ef1bcc00258d78e9d7112f0cbce
Sam Brenny <Sam Brenny>
2023-09-25 16:00:11 -0500

    Updated UI with all input/output parameters

--- BackgroundNoiseSupression/BackgroundNoiseSupression.py
+++ BackgroundNoiseSupression/BackgroundNoiseSupression.py
@@ -29,7 +29,7 @@ class BackgroundNoiseSupression(ScriptedLoadableModule):
         self.parent.title = "BackgroundNoiseSupression"  # TODO: make this more human readable by adding spaces
         self.parent.categories = ["Filtering"]  # TODO: set categories (folders where the module shows up in the module selector)
         self.parent.dependencies = []  # TODO: add here list of module names that this module requires
-        self.parent.contributors = ["John Doe (AnyWare Corp.)"]  # TODO: replace with "Firstname Lastname (Organization)"
+        self.parent.contributors = ["Sam Brenny UMN CMRR)"]  # TODO: replace with "Firstname Lastname (Organization)"
         # TODO: update with short description of the module and a link to online module documentation
         self.parent.helpText = """
 This is an example of scripted loadable module bundled in an extension.
